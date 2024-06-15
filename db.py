34d1ada62d09a8f2e78bbb75350e2a168479fe70
Hojin Jang <23309485+jhojin7@users.noreply.github.com>
2024-06-15 22:16:45 +0900

    first commit

---
+++ db.py
@@ -0,0 +1,36 @@
+import sqlite3
+import time
+
+def get_all_tasks(sort_by_newest=False)->list:
+    with sqlite3.connect("database.db") as conn:
+        cursor = conn.cursor()
+        keys = "rowid content created_at".split()
+        q = f"""select {','.join(keys)} from tasks """
+        if sort_by_newest:
+            q += "order by created_at desc"
+        res = conn.cursor().execute(q)
+        ret = []
+        for task in res.fetchall():
+            d = {}
+            for i,key in enumerate(keys):
+                d[key] = task[i] 
+            ret.append(d)
+        return ret
+    
+def add_one_task(content:str)->int:
+    with sqlite3.connect("database.db") as conn:
+        cursor = conn.cursor()
+        res = cursor.execute(f"insert into tasks(content, created_at) values (?,?)", (str(content), int(time.time())))
+        print(res.fetchone())
+        return 0
+
+if __name__=="__main__":
+    with sqlite3.connect("database.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute("""create table if not exists tasks 
+                        (created_at integer,
+                        content text)""")
+        # tasks =  [(f'{i}',int(time.time()), f"task {i}") for i in range(10)]
+        tasks =  [(int(time.time()), f"task {i}") for i in range(10)]
+        cursor = conn.cursor()
+        cursor.executemany("insert into tasks(created_at,content) values(?,?)",tasks)
---
+++ server.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, Body, Request, Form
+from fastapi.responses import HTMLResponse, FileResponse, JSONResponse, RedirectResponse
+from db import get_all_tasks, add_one_task
+from typing import Annotated
+
+app = FastAPI()
+
+@app.get("/")
+def main():
+    return FileResponse("index.html")
+
+@app.get("/tasks")
+def list_tasks():
+    tasks = get_all_tasks(sort_by_newest=True)
+    return JSONResponse(tasks,200)
+
+@app.post("/tasks")
+def add_task(
+    content:Annotated[str, Form()]
+):
+    resp = add_one_task(content)
+    if resp:
+        return JSONResponse({"error":str(resp),"content":str(content)},200)
+    return HTMLResponse("index.html")
